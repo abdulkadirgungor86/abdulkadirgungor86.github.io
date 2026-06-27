6d081bee4c4b693c56a128ecf505fad7e9f4f0a4
Abdulkadir Güngör <abdulkadir.gungor@outlook.com>
2026-06-27 22:54:06 +0300

    updated head.html

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Build script for Vercel (or local production build)
-
-git submodule update --init --recursive
-hugo --minify
--- dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-hugo server --config config.toml,config.local.toml --baseURL "http://localhost:1313/" --disableFastRender
